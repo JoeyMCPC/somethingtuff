b4a10f36668e187e02407c1df04087fc8b95c4b5
JoeyMCPC <142174364+JoeyMCPC@users.noreply.github.com>
2025-05-04 19:59:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-$conn = new mysqli("localhost", "root", "elm600.root", "imageboard");
+// I think you get the point
+$conn = new mysqli
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
