c4c5f0220686b439b050eb609cc27e6e13d4db9b
JoeyMCPC <142174364+JoeyMCPC@users.noreply.github.com>
2025-05-04 20:00:20 -0400

    Update post_announcement.php

--- post_announcement.php
+++ post_announcement.php
@@ -4,7 +4,7 @@ if (!isset($_SESSION['admin'])) {
     header("Location: admin_login.php");
     exit;
 }
-$conn = new mysqli("localhost", "root", "elm600.root", "imageboard");
+//Trust the process 
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
 
 $msg = $_POST['message'];
